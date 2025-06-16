f62bad12e3637877dd57354b8b2a78b3ed3c155e
axebor <axebo706@gmail.com>
2025-06-16 14:29:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,27 @@ import streamlit as st
 
 st.set_page_config(page_title="Gravitationsfundament", layout="wide")
 
-st.header("Indata")
-st.subheader("Geometri")
+# Två kolumner
+col_in, col_out = st.columns(2)
 
-# Diameter bottenplatta
-D_b = st.text_input(" ", value="5.0", key="D_b")
-st.latex(r"D_b = \text{" + D_b + r"\text{ m}}")
+# VÄNSTERKOLUMN – INDATA
+with col_in:
+    st.header("Indata")
+    st.subheader("Geometri")
 
-# Höjd bottenplatta
-h_b = st.text_input(" ", value="1.0", key="h_b")
-st.latex(r"h_b = \text{" + h_b + r"\text{ m}}")
+    st.latex(r"D_b = \text{Diameter bottenplatta (m)}")
+    D_b = st.number_input(label="", value=5.0, step=0.1, format="%.1f", key="D_b")
 
-# Diameter skaft
-D_s = st.text_input(" ", value="1.0", key="D_s")
-st.latex(r"D_s = \text{" + D_s + r"\text{ m}}")
+    st.latex(r"h_b = \text{Höjd bottenplatta (m)}")
+    h_b = st.number_input(label="", value=1.0, step=0.1, format="%.1f", key="h_b")
 
-# Höjd skaft
-h_s = st.text_input(" ", value="2.0", key="h_s")
-st.latex(r"h_s = \text{" + h_s + r"\text{ m}}")
+    st.latex(r"D_s = \text{Diameter skaft (m)}")
+    D_s = st.number_input(label="", value=1.0, step=0.1, format="%.1f", key="D_s")
+
+    st.latex(r"h_s = \text{Höjd skaft (m)}")
+    h_s = st.number_input(label="", value=2.0, step=0.1, format="%.1f", key="h_s")
+
+# HÖGERKOLUMN – RESULTAT
+with col_out:
+    st.header("Resultat")
+    st.info("Resultat kommer att visas här.")
