9c5a033b44ed220a7c2d9116dfa9a8044dedc8d5
axebor <axebo706@gmail.com>
2025-06-16 13:44:28 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# Geometri
+st.subheader("Geometri")
+
+# Bottenplatta
+st.markdown("**Bottenplatta**")
+D_b = st.number_input("Diameter D\u2090 (m)", min_value=0.5, value=5.0, step=0.1)
+h_b = st.number_input("Höjd h\u2090 (m)", min_value=0.1, value=1.0, step=0.1)
+
+# Skaft
+st.markdown("**Skaft (centrerat ovanpå)**")
+D_s = st.number_input("Diameter D\u209B (m)", min_value=0.1, max_value=D_b, value=1.0, step=0.1)
+h_s = st.number_input("Höjd h\u209B (m)", min_value=0.1, value=2.0, step=0.1)
