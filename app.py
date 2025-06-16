6e36bbea744d8a297eb7e5ca2f0e5e72945b5a82
axebor <axebo706@gmail.com>
2025-06-16 14:13:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Gravitationsfundament", layout="wide")
 
-# Två kolumner
 col_in, col_out = st.columns(2)
 
-# Vänsterkolumn – Indata
 with col_in:
     st.header("Indata")
     st.subheader("Geometri")
@@ -17,41 +15,36 @@ with col_in:
     col3.markdown("**Värde**")
     col4.markdown("**Enhet**")
 
-    # --- Diameter bottenplatta ---
+    # Indata-rader
     with st.container():
         col1, col2, col3, col4 = st.columns([2.5, 1, 1.2, 1])
         col1.write("Diameter bottenplatta")
         col2.markdown("$D_b$")
-        D_b = col3.number_input(label="", value=5.0, step=0.1, format="%.1f", key="D_b")
+        D_b = float(col3.text_input(label="", value="5.0", key="D_b"))
         col4.write("m")
 
-    # --- Höjd bottenplatta ---
     with st.container():
         col1, col2, col3, col4 = st.columns([2.5, 1, 1.2, 1])
         col1.write("Höjd bottenplatta")
         col2.markdown("$h_b$")
-        h_b_str = col3.text_input(label="", value="1.0", key="h_b")
+        h_b = float(col3.text_input(label="", value="1.0", key="h_b"))
         col4.write("m")
 
-    # --- Diameter skaft ---
     with st.container():
         col1, col2, col3, col4 = st.columns([2.5, 1, 1.2, 1])
         col1.write("Diameter skaft")
         col2.markdown("$D_s$")
-        D_s_str = col3.text_input(label="", value="1.0", key="D_s")
+        D_s = float(col3.text_input(label="", value="1.0", key="D_s"))
         col4.write("m")
 
-    # --- Höjd skaft ---
     with st.container():
         col1, col2, col3, col4 = st.columns([2.5, 1, 1.2, 1])
         col1.write("Höjd skaft")
         col2.markdown("$h_s$")
-        h_s_str = col3.text_input(label="", value="2.0", key="h_s")
+        h_s = float(col3.text_input(label="", value="2.0", key="h_s"))
         col4.write("m")
 
-    # --- Konvertera till tal ---
-
-# Högerkolumn – Resultat (tom för nu)
+# Högerkolumn – tom ännu
 with col_out:
     st.header("Resultat")
     st.info("Resultat kommer att visas här.")
